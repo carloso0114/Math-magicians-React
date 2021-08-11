c96a401347f9566c043956fe086e332071f53458
carloso0114 <carlossospinag@gmail.com>
2021-08-11 02:41:43 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import React from 'react';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
