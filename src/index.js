b1273fbbf1d8f26a053e54c28d692434791ed49f
carloso0114 <carlossospinag@gmail.com>
2021-08-11 01:49:13 -0500

    import calculator on App.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Calculator from './components/calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
