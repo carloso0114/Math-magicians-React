9b35cb983f19de84d1977532eb4cfa4eb55cf0c4
carloso0114 <carlossospinag@gmail.com>
2021-08-20 01:17:31 -0500

    add navlinks to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
 import React from 'react';
-import Calculator from './components/calculator';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Calculator from './components/Calculator';
+import Quote from './pages/Quote';
 
 const App = () => (
-  <div>
-    <Calculator />
-  </div>
+  <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <div>
+          <Route path="/calculator">
+
+            <Calculator />
+
+          </Route>
+          <Route path="/quote">
+            <Quote />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </div>
+      </Switch>
+    </Router>
+  </>
 );
 
 export default App;
